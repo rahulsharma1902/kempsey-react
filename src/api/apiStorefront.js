356f68348a7fa5f9b27446a76222e36d14ba996c
HiteshRajput00 <hit8708542@gmail.com>
2024-09-03 18:46:40 +0530

    first commit

--- src/api/apiStorefront.js
+++ src/api/apiStorefront.js
@@ -29,6 +29,52 @@ export const sitecontent = () => fetchData('site-content');
 export const getProductById = (id) => fetchData(`get-product/${id}`);
 export const products = () => fetchData(`products`);
 
+export const Faqs = () => fetchData('faqs');
+export const getfaqId = (id) => fetchData(`get-faq/${id}`);
+
+export const Faqcategories = () => fetchData('faq-categories');  
+export const getfaqcategoryId = (id) => fetchData(`get-faq-category/${id}`);
+
+
+export const removeFaq = async (id) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.post(`${API_URL}/faq/remove/${id}`, {},
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            }
+        );
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        const errorMessage = error.response?.data?.message || 'Failed to remove category.';
+        // toast.error(errorMessage);
+        throw new Error(errorMessage);
+    }
+};
+
+export const removeFaqCategory = async (id) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.post(`${API_URL}/faq-category/remove/${id}`, {},
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            }
+        );
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        const errorMessage = error.response?.data?.message || 'Failed to remove category.';
+        // toast.error(errorMessage);
+        throw new Error(errorMessage);
+    }
+};
 
 
 
@@ -61,5 +107,7 @@ export const addHomeContent = (formData) => postData('home-content/add', formDat
 export const addAboutUsContent = (formData) => postData('about-us-content/add', formData, true);
 export const addStoreContent = (formData) => postData('store-content/add', formData, true);
 export const updateProduct = (formData) => postData('product/update', formData, true);
+export const addfaqcategory = (formData) => postData('faq-category/add', formData, true);
+export const addfaq = (formData) => postData('faq/add', formData, true);
 
 export const removeProduct = (id) => postData(`product/remove/${id}`, {});
